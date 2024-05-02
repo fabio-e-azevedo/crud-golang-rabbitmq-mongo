c5cb361781aa6dd258ba29d78bc0408e8eb795c5
Fabio Azevedo <emaildofabioeduardo@gmail.com>
2024-05-02 03:53:43 -0300

    update command line sub-command delete

--- cmd/cli/delete.go
+++ cmd/cli/delete.go
@@ -11,71 +11,143 @@ import (
 
 func init() {
 	rootCmd.AddCommand(deleteCmd)
+	deleteCmd.AddCommand(albumsCmd)
+	deleteCmd.AddCommand(commentsCmd)
+	deleteCmd.AddCommand(photosCmd)
+	deleteCmd.AddCommand(postsCmd)
+	deleteCmd.AddCommand(todosCmd)
+	deleteCmd.AddCommand(usersCmd)
 }
 
 var deleteCmd = &cobra.Command{
 	Use:   "delete",
 	Short: "Delete Resource by ID",
-
 	Run: func(cmd *cobra.Command, args []string) {
-		if len(args) == 0 {
-			fmt.Println("enter the name of a resource: albums, comments, photos, posts, todos, users")
-			os.Exit(1)
-		}
+		cmd.Help()
+	},
+}
 
-		if len(args) == 1 {
-			fmt.Println("enter the resource id")
-			os.Exit(1)
-		}
+func deleteResourceById(resourceType string, resourceId int) {
+	url := fmt.Sprintf("http://localhost:5000/api/v1/%s/%d", resourceType, resourceId)
+	req, err := http.NewRequest("DELETE", url, nil)
+	if err != nil {
+		fmt.Println("error request delete:", err)
+		os.Exit(1)
+	}
 
-		resourceType := args[0]
+	client := http.Client{}
 
-		valueID, err := strconv.Atoi(args[1])
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("error send request delete:", err)
+		os.Exit(1)
+	}
+	defer resp.Body.Close()
+
+	// body, err := io.ReadAll(resp.Body)
+	// if err != nil {
+	// 	fmt.Println("error reading response body:", err)
+	// 	return
+	// }
+
+	if resp.StatusCode == 404 {
+		fmt.Printf("not found id %d\n", resourceId)
+		return
+	}
+
+}
+
+var albumsCmd = &cobra.Command{
+	Use:   "albums [id] or",
+	Short: "Resource albums",
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		// if len(args) < 1 {
+		// 	fmt.Println("enter the resource id")
+		// 	return
+		// }
+
+		albumsID, err := strconv.Atoi(args[0])
 		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
+			fmt.Println("the albums id must be a number")
+			return
 		}
 
-		resources := map[string]bool{
-			"albums":   true,
-			"comments": true,
-			"photos":   true,
-			"posts":    true,
-			"todos":    true,
-			"users":    true,
+		deleteResourceById("albums", albumsID)
+	},
+}
+
+var commentsCmd = &cobra.Command{
+	Use:   "comments [id] or",
+	Short: "Resource comments",
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		commentsID, err := strconv.Atoi(args[0])
+		if err != nil {
+			fmt.Println("the comments id must be a number")
+			return
 		}
 
-		if resources[resourceType] {
-			url := fmt.Sprintf("http://localhost:5000/api/v1/%s/%d", resourceType, valueID)
-			req, err := http.NewRequest("DELETE", url, nil)
-			if err != nil {
-				fmt.Println("error request delete:", err)
-				os.Exit(1)
-			}
+		deleteResourceById("comments", commentsID)
+	},
+}
 
-			client := http.Client{}
-
-			resp, err := client.Do(req)
-			if err != nil {
-				fmt.Println("error send request delete:", err)
-				os.Exit(1)
-			}
-			defer resp.Body.Close()
-
-			// body, err := io.ReadAll(resp.Body)
-			// if err != nil {
-			// 	fmt.Println("error reading response body:", err)
-			// 	return
-			// }
-
-			if resp.StatusCode == 404 {
-				fmt.Printf("not found id %d\n", valueID)
-				os.Exit(1)
-			}
-		} else {
-			fmt.Printf("not found resource \"%s\"\n", resourceType)
-			fmt.Println("enter the name of a resource: albums, comments, photos, posts, todos, users")
-			os.Exit(1)
+var photosCmd = &cobra.Command{
+	Use:   "photos [id] or",
+	Short: "Resource photos",
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		photosID, err := strconv.Atoi(args[0])
+		if err != nil {
+			fmt.Println("the photos id must be a number")
+			return
 		}
+
+		deleteResourceById("photos", photosID)
+	},
+}
+
+var postsCmd = &cobra.Command{
+	Use:   "posts [id] or",
+	Short: "Resource posts",
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		postsID, err := strconv.Atoi(args[0])
+		if err != nil {
+			fmt.Println("the posts id must be a number")
+			return
+		}
+
+		deleteResourceById("posts", postsID)
+	},
+}
+
+var todosCmd = &cobra.Command{
+	Use:   "todos [id] or",
+	Short: "Resource todos",
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		todosID, err := strconv.Atoi(args[0])
+		if err != nil {
+			fmt.Println("the todos id must be a number")
+			return
+		}
+
+		deleteResourceById("todos", todosID)
+	},
+}
+
+var usersCmd = &cobra.Command{
+	Use:   "users [id] or",
+	Short: "Resource users",
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		userID, err := strconv.Atoi(args[0])
+		if err != nil {
+			fmt.Println("the users id must be a number")
+			return
+		}
+
+		deleteResourceById("users", userID)
 	},
 }
